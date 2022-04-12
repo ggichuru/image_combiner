860689c13fefe9f5be627f2d00906fe93bf0b8ca
ggichuru <ggichuru.dev@gmail.com>
2022-04-12 21:40:33 +0300

    fn [combine images, alternate_pixels, get_dimensions]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,109 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+use args::Args;
+use image::{
+    imageops::FilterType::Triangle, io::Reader, DynamicImage, GenericImageView, ImageFormat,
+};
+use std::{fs::File, io::BufReader};
+
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    println!("{:?}", args);
+
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+    let combined_data = combine_images(image_1, image_2);
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimensions(image_1.dimensions(), image_2.dimensions());
+    println!("witdth: {}, height: {}", width, height);
+
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, height, Triangle))
+    }
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity = 3_655_744;
+        let buffer: Vec<u8> = Vec::with_capacity(buffer_capacity);
+        FloatingImage {
+            width,
+            height,
+            data: buffer,
+            name,
+        }
+    }
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgb8().into_vec();
+    let vec_2 = image_2.to_rgb8().into_vec();
+
+    alteranate_pixels(vec_1, vec_2)
+}
+
+fn alteranate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let mut combined_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+        } else {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+        }
+
+        i += 4;
+    }
+
+    combined_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba = Vec::new();
+
+    for i in start..=end {
+        let val = match vec.get(i) {
+            Some(val) => *val,
+            None => panic!("Index out of bounds"),
+        };
+    }
+
+    rgba
 }
